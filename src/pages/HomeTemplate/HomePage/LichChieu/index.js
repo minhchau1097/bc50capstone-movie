571209859fe895ce2126c55c62a5748635a2d01c
nguyennguyenthanh <thanhnguyennguyen1228@gmail.com>
2023-08-07 12:37:14 +0700

    Reponsive LichChieu

--- src/pages/HomeTemplate/HomePage/LichChieu/index.js
+++ src/pages/HomeTemplate/HomePage/LichChieu/index.js
@@ -12,7 +12,7 @@ class LichChieu extends Component {
 
   renderListLichChieu = () => {
     const { data } = this.props;
-    return data?.map((movie) => <div key={movie.maPhim}><LichChieuItem movie={movie} /></div>)
+    return data?.map((movie) => <div key={movie.maPhim} ><LichChieuItem movie={movie} /></div>)
   }
   handlePopUp = () => {
     Swal.fire({
@@ -31,13 +31,38 @@ class LichChieu extends Component {
       rows: 2,
       slidesPerRow: 4,
       arrows: false,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesPerRow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 960,
+          settings: {
+            slidesPerRow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 599,
+          settings: {
+            slidesPerRow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
     };
     return (
       <div className='container'>
-        <div className="headerTitle titleLichChieu">
+        <div className="titleLichChieu">
           <div className='row container'>
             <div className="under-line col-md-3">
-              <div className="form-group mt-4">
+              <div className="form-group">
                 <select className="form-control" name="">
                   <option>Phim</option>
                   <option></option>
@@ -45,7 +70,7 @@ class LichChieu extends Component {
               </div>
             </div>
             <div className="under-line col-md-3 partition">
-              <div className="form-group mt-4">
+              <div className="form-group">
                 <select className="form-control" name="">
                   <option>Rạp</option>
                   <option></option>
@@ -54,7 +79,7 @@ class LichChieu extends Component {
               </div>
             </div>
             <div className="under-line col-md-3 partition">
-              <div className="form-group mt-4">
+              <div className="form-group">
                 <select className="form-control" name="">
                   <option>Ngày giờ chiếu</option>
                   <option></option>
@@ -64,15 +89,27 @@ class LichChieu extends Component {
             </div>
             <div className='col-md-3 partition'>
               {/* dùng sweet alert để hiện box */}
-              <button className="btnMuaVe" onClick={() => {
+              <button className="btn btnMuaVe" onClick={() => {
                 this.handlePopUp()
               }}>MUA VÉ NGAY</button>
             </div>
           </div>
         </div>
-        <Slider {...settings}>
-          {this.renderListLichChieu()}
-        </Slider>
+        {/* style={{display: "none"}} */}
+        <nav className="navbar navbar-expand-sm navbar-dark nav-search container" style={{ width: "85%" }} >
+          <form className="form-inline">
+            <div className="row w-100">
+              <div className="col-10">
+                <input className="input-search form-control mr-sm-2" type="text" placeholder="Search Movie" style={{ border: "none" }} /></div>
+              <div className="col-2">
+                <img className='img-search img-fluid' src="./images/search.png" alt="image-search" />
+              </div>
+            </div>
+          </form>
+        </nav>
+          <Slider {...settings}>
+            {this.renderListLichChieu()}
+          </Slider>
         <div className='container mt-5'>
           <img src="./images/shadow.png" alt="" width={1080} />
         </div>
