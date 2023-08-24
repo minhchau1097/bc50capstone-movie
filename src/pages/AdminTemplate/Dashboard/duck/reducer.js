1ba411269a7fabcec4a2fa00528167eff6ba4a86
nguyennguyenthanh <thanhnguyennguyen1228@gmail.com>
2023-08-24 11:27:57 +0700

    Upload-Delete-Search-AdminDashboard

--- src/pages/AdminTemplate/Dashboard/duck/reducer.js
+++ src/pages/AdminTemplate/Dashboard/duck/reducer.js
@@ -70,6 +70,25 @@ const manageUserReducer = (state = manageUser, action) => {
             state.thongTinNguoiDung = action.payload;
             return {...state};
         }
+        //DELETE
+        case ActionType.DELETE_USER_REQUEST: {
+            state.loading = true;
+            state.data = null;
+            state.error = null;
+            return { ...state };
+        }
+        case ActionType.DELETE_USER_SUCCESS: {
+            state.loading = false;
+            state.data = action.payload;
+            state.error = null;
+            return { ...state };
+        }
+        case ActionType.DELETE_USER_FAIL: {
+            state.loading = false;
+            state.data = null;
+            state.error = action.payload;
+            return { ...state };
+        }
         default:
             return { ...state };
     }
