2484c9b3b77a9921d84bc161c2fed250220eab88
nguyennguyenthanh <thanhnguyennguyen1228@gmail.com>
2023-08-24 23:11:52 +0700

    Upload-Home-LichChieu1

--- src/pages/HomeTemplate/HomePage/LichChieu/duck/constants.js
+++ src/pages/HomeTemplate/HomePage/LichChieu/duck/constants.js
@@ -2,6 +2,10 @@ export const LICHCHIEU_REQUEST = "@@carouselReducer/LICHCHIEU_REQUEST";
 export const LICHCHIEU_SUCCESS = "@@carouselReducer/LICHCHIEU_SUCCESS";
 export const LICHCHIEU_FAIL = "@@carouselReducer/LICHCHIEU_FAIL";
 
-export const INFO_LICHCHIEU_REQUEST = "@@carouselReducer/INFO_LICHCHIEU_REQUEST";
-export const INFO_LICHCHIEU_SUCCESS = "@@carouselReducer/INFO_LICHCHIEU_SUCCESS";
-export const INFO_LICHCHIEU_FAIL = "@@carouselReducer/INFO_LICHCHIEU_FAIL";
+export const INFO_HT_RAP_REQUEST = "@@carouselReducer/INFO_HT_RAP_REQUEST";
+export const INFO_HT_RAP_SUCCESS = "@@carouselReducer/INFO_HT_RAP_SUCCESS";
+export const INFO_HT_RAP_FAIL = "@@carouselReducer/INFO_HT_RAP_FAIL";
+
+export const INFO_CUM_RAP_REQUEST = "@@carouselReducer/INFO_CUM_RAP_REQUEST";
+export const INFO_CUM_RAP_SUCCESS = "@@carouselReducer/INFO_CUM_RAP_SUCCESS";
+export const INFO_CUM_RAP_FAIL = "@@carouselReducer/INFO_CUM_RAP_FAIL";
--- src/pages/HomeTemplate/HomePage/LichChieu/duck/reducer.js
+++ src/pages/HomeTemplate/HomePage/LichChieu/duck/reducer.js
@@ -1,44 +1,66 @@
-import { LICHCHIEU_REQUEST, LICHCHIEU_SUCCESS, LICHCHIEU_FAIL, INFO_LICHCHIEU_REQUEST, INFO_LICHCHIEU_SUCCESS, INFO_LICHCHIEU_FAIL } from './constants';
+import * as ActionTypes from './constants';
 
 const stateLichChieu = {
   loading: false,
   data: null,
-  error: null
+  error: null,
+  heThongRapChieu: [],
+  cumRapChieu: []
 }
 
 const lichChieuReducer = (state = stateLichChieu, action) => {
   switch (action.type) {
-    case LICHCHIEU_REQUEST: {
+    case ActionTypes.LICHCHIEU_REQUEST: {
       state.loading = true;
       state.data = null;
       state.error = null;
       return { ...state };
     }
-    case LICHCHIEU_SUCCESS: {
+    case ActionTypes.LICHCHIEU_SUCCESS: {
       state.loading = false;
       state.data = action.payload;
       state.error = null;
       return { ...state };
     }
-    case LICHCHIEU_FAIL: {
+    case ActionTypes.LICHCHIEU_FAIL: {
       state.loading = false;
       state.data = null;
       state.error = action.payload;
       return { ...state };
     }
-    case INFO_LICHCHIEU_REQUEST: {
+    //HT RẠP
+    case ActionTypes.INFO_HT_RAP_REQUEST: {
       state.loading = true;
       state.data = null;
       state.error = null;
       return { ...state };
     }
-    case INFO_LICHCHIEU_SUCCESS: {
+    case ActionTypes.INFO_HT_RAP_SUCCESS: {
+      state.loading = false;
+      state.heThongRapChieu = action.payload;
+      state.error = null;
+      return { ...state };
+    }
+    case ActionTypes.INFO_HT_RAP_FAIL: {
+      state.loading = false;
+      state.data = null;
+      state.error = action.payload;
+      return { ...state };
+    }
+    //CUM RẠP
+    case ActionTypes.INFO_CUM_RAP_REQUEST: {
+      state.loading = true;
+      state.data = null;
+      state.error = null;
+      return { ...state };
+    }
+    case ActionTypes.INFO_CUM_RAP_SUCCESS: {
       state.loading = false;
       state.data = action.payload;
       state.error = null;
       return { ...state };
     }
-    case INFO_LICHCHIEU_FAIL: {
+    case ActionTypes.INFO_CUM_RAP_FAIL: {
       state.loading = false;
       state.data = null;
       state.error = action.payload;
