a245df9d206888397fd28f99b312863ae8f46e02
minhchau1097 <minhchau1097@gmail.com>
2023-08-27 14:19:45 +0700

    timeout

--- src/pages/HomeTemplate/index.js
+++ src/pages/HomeTemplate/index.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Navbar from './_components/navbar'
+import { actTryLogin } from 'pages/AdminTemplate/LoginPage/duck/actions'
+import { useDispatch } from 'react-redux'
 
 export default function HomeTemplate() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   useEffect(() => {
     window.scrollTo(0, 0)
   })
+  useEffect(()=>{
+    dispatch(actTryLogin(navigate))
+  },[])
   return (
     <>
       <Navbar />
